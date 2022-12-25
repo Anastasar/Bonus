0548b615d8cbd463dce7e6027325e8ca3eb42ea0
Anastasar <ereminaar83@mail.ru>
2022-12-25 17:38:23 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        int x= 100;
-        int i= 100;
+        int intial_account = 100;
+        int deposit_amount = 2000;
 
-        if (i > 1000) {
-            System.out.println("Сумма бонуса составит:     " + i / 100);
-            System.out.println("Итоговая сумма счета составит:        " + (x + i + i / 100));
+        if (deposit_amount > 1000) {
+            System.out.println("Сумма бонуса составит:     " + deposit_amount / 100);
+            System.out.println("Итоговая сумма счета составит:        " + (intial_account + deposit_amount + deposit_amount / 100));
         } else {
             System.out.println("Сумма бонуса составит: 0 рублей");
-            System.out.println("Итоговая сумма счета составит:        " + (x + i));
+            System.out.println("Итоговая сумма счета составит:        " + (intial_account + deposit_amount));
         }
 
     }
